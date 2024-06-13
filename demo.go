c2ac0721199628587c57faa506350df781549d7b
mayusB <54024278+mayusB@users.noreply.github.com>
2024-06-13 04:22:52 +0100

    Create demo.go

---
+++ demo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"crypto/cipher"
+	"crypto/des"
+	"crypto/md5"
+	"crypto/rand"
+	"crypto/rc4"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/cgi"
+	"os"
+)
+
+func main1() {
+	// ruleid: insecure-module-used
+	cgi.Serve(http.FileServer(http.Dir("/usr/share/doc")))
+}
+
