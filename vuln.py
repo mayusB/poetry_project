d18c4d30c08946272b026ba6060114a4041d07f3
mayusB <54024278+mayusB@users.noreply.github.com>
2024-09-11 10:56:05 +0100

    Update vuln.py

--- vuln.py
+++ vuln.py
@@ -3,14 +3,3 @@ from sqlalchemy.orm import relationship
 
 
 @api_view(["GET", "POST"])
-def snippet_list(request):
-    tainted = request.GET["query"]
-
-    from .models import Child
-    from .models import Parent
-
-    # ruleid: sqlalchemy-django-relationship
-    Parent.children = relationship(Child, primaryjoin=f"Parent.id == {tainted}")
-
-    # ruleid: sqlalchemy-django-relationship
-    Parent.children = relationship(Child, foreign_keys=f"Parent.id == {tainted}")
